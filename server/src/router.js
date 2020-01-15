6c0121bd0832000afad054e926406d150b41527e
Vlad Ventura <vladventura@outlook.es>
2020-01-15 18:36:57 -0500

    Restructuring the web app as a whole

--- server/src/router.js
+++ server/src/router.js
@@ -3,7 +3,7 @@ const url = require('url');
 const formidable = require('formidable');
 const querystring = require('querystring');
 
-const imagesFolder = "./data/images/";
+const imagesFolder = "../data/images/";
 
 function home(request, response) {
     var urlObject = url.parse(`${request.headers.host}${request.url}`);
@@ -13,10 +13,10 @@ function home(request, response) {
         "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
         "Access-Control-Max-Age": 2592000 // 30 days
         /** add other headers as per requirement */
-      };
+    };
     if (urlObject.pathname === '/') {
         if (request.method == "GET") {
-            var file = fs.readFileSync("../client/index.html");
+            var file = fs.readFileSync("../../client/index.html");
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(file);
             response.end();
@@ -48,52 +48,68 @@ function home(request, response) {
     } else if (urlObject.pathname === "/upload") {
         if (request.method == "GET") {
             response.writeHead(200, { 'Content-Type': 'text/html' });
-            fs.createReadStream('../client/form_page/form.html').pipe(response);
+            fs.createReadStream('../../client/form_page/form.html').pipe(response);
         } else if (request.method == "POST") {
             var form = new formidable.IncomingForm();
             form.keepExtensions = true;
             form.parse(request, function (error, fields, files) {
                 console.log(fields);
                 console.log(files.picture.name);
-                response.write("File Uploaded and Saved!");
-                response.end();
-                // var oldPath = files.filetoupload.path;
-                // var newPath = `${imagesFolder}/${files.filetoupload.name}`;
-                // fs.rename(oldPath, newPath, error => {
-                //   if (error) throw error;
-                // });
+                var oldPath = files.filetoupload.path;
+                var newPath = `${imagesFolder}/${files.filetoupload.name}`;
+                fs.rename(oldPath, newPath, error => {
+                    if (error) throw error;
+                    response.write("File Uploaded and Saved!");
+                    response.end();
+                });
             });
         }
     } else if (urlObject.pathname === "/today") {
-        if (response.method == "GET") {
+        console.log("Get the /today");
+        if (request.method == "GET") {
             response.writeHead(200, { 'Content-Type': 'text/html' });
-            fs.createReadStream('../client/slideshow_page/slideshow.html').pipe(response);
+            fs.createReadStream('../../client/slideshow_page/slideshow.html').pipe(response);
         }
     } else {
         if (request.method === "OPTIONS") {
             response.writeHead(200, headers);
             response.end();
             return;
-          } else {
+        } else {
             response.end("Undefined request");
-          }
+        }
     }
 }
 
 function css(request, response) {
-    console.log(request.url);
     if (request.url.indexOf(".css") !== -1) {
         var name = request.url.split(".")[0];
         var file;
-        console.log(name);
-        if (name === "/index"){
-            file = fs.readFileSync(`../client${request.url}`, { 'encoding': 'utf8' });
+        if (name === "/index") {
+            file = fs.readFileSync(`../../client${request.url}`, { 'encoding': 'utf8' });
         } else {
-            file = fs.readFileSync(`../client${name}_page${request.url}`, { 'encoding': 'utf8' });
+            file = fs.readFileSync(`../../client${name}_page${request.url}`, { 'encoding': 'utf8' });
         }
         response.writeHead(200, { 'Content-Type': 'text/css' });
         response.write(file);
     }
 }
- module.exports.home = home;
- module.exports.css = css;
+
+function js(request, response) {
+    console.log(request.url + "on js open");
+    if (request.url.indexOf(".js") !== -1) {
+        var name = request.url.split(".")[0];
+        var file;
+        if (name === "/index") {
+            file = fs.readFileSync(`../../client${request.url}`, { 'encoding': 'utf8' });
+        } else {
+            file = fs.readFileSync(`../../client${name}_page${request.url}`, { 'encoding': 'utf8' });
+        }
+        response.writeHead(200, { 'Content-Type': 'text/javascript' });
+        response.write(file);
+    }
+}
+
+module.exports.home = home;
+module.exports.css = css;
+module.exports.js = js;
