bdb99e91114eafbba79d0c3004608155ee953506
Vlad Ventura <vladventura@outlook.es>
2020-01-17 19:39:17 -0500

    I'm personally happy with how the app is going right now because we've been able to keep track of the pictures, their dates, when the list was built and if it has been built previously to today, then we should build a new one.

--- server/src/dbmanager.js
+++ server/src/dbmanager.js
@@ -3,30 +3,52 @@ const fs = require('fs');
 const imagesFolder = "./data/images/";
 const dbLocation = "./data/db.json";
 
+let db = fs.readFileSync(dbLocation);
+db = JSON.parse(db);
+
 const buildToday = () => {
-    let jsonFile = fs.readFileSync("./data/db.json");
-    jsonFile = JSON.parse(jsonFile);
-    return jsonFile;
+    var todayList = [];
+    fs.readdir(imagesFolder, (err, files) => {
+        var today = new Date();
+        files.forEach(image => {
+            var imageDate = new Date(db.entries[image].dates);
+            if (imageDate <= today){
+                todayList.push(image);
+            }
+        });
+        db.metadata["todayList"] = todayList;
+        db.metadata["dateBuilt"] = today;
+        let jsonData = JSON.stringify(db);
+        fs.writeFileSync(dbLocation, jsonData);
+        return todayList;
+    });
+}
+
+const getTodayList = () => {
+    var today = new Date();
+    var built = new Date(db.metadata["dateBuilt"]);
+
+    if (built < today){
+        return buildToday();
+    } else {
+        return db.metadata["todayList"];
+    }
 }
 
 const initialize = () => {
-    let db = fs.readFileSync(dbLocation);
-    db = JSON.parse(db);
     fs.readdir(imagesFolder, (err, files) => {
         files.forEach(image => {
             db.entries[image] = {
                 "name": "Vladimir",
                 "lastname": "Ventura",
                 "studentID": "00301144",
-                "dates": "02-12-2020",
+                "dates": "2020-01-11",
                 "pictureName": image
             };
         });
-        db.metadata.imageNumber++;
         let jsonData = JSON.stringify(db);
         fs.writeFileSync(dbLocation, jsonData);
     });
 }
 
-initialize();
-module.exports.buildToday = buildToday;
+module.exports.getTodayList = getTodayList;
