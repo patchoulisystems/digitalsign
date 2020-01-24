be2fd3446db5c95fa30b13bf6f44f1fa9111611d
Vlad Ventura <vladventura@outlook.es>
2020-01-24 18:07:33 -0500

    Adding the code for the widget's functionality here since it just makes things work.

--- client/form_page/form.js
+++ client/form_page/form.js
@@ -1,57 +1,43 @@
-const url = "127.0.0.1:3000";
-const endpoint = "/upload";
-
-//Another approach to handling the form would be...
-const submitForm = () => {
-  var form = document.getElementById('main-form');
-  form.setAttribute('action', `${url}${endpoint}`);
+var options = {
+    rangeSelect: true,
+    minDate: 0,
+    multiSelect: 0,
+    dateFormat: 'yyyy-mm-dd',
+    altField: '#dpfield'
 };
 
-
-const handleSubmit = event => {
-  // Stops the form from submitting; we're doing that with AJAX
-  event.preventDefault();
-  // Call our function to get the form data here
-  console.log(event);
-  const data = formToJSON(form.elements);
-  console.log(form.elements);
-  console.log(data);
-  postSubmit(data);
-};
-
-async function postSubmit(data) {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-    "Access-Control-Max-Age": 2592000 // 30 days
-    /** add other headers as per requirement */
-  };
-  const response = await fetch(`http://${url}/${endpoint}`, {
-    method: "POST",
-    body: data,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-      "Access-Control-Max-Age": 2592000 // 30 days,
-    },
-  });
+const enableDatepicker = () => {
+    $("input[name='radio']").on('change', function() {
+        var selected = $("input[name='radio']:checked").val();
+        switch (selected) {
+            case 'multiple':
+                options.rangeSelect = null;
+                options.multiSelect = 999;
+                $('#datepicker').datepick('destroy').datepick(options);
+                $('#dpfield').val('');
+                break;
+            case 'interval':
+                options.rangeSelect = true;
+                options.multiSelect = null;
+                $('#datepicker').datepick('destroy').datepick(options);
+                $('#dpfield').val('');
+                break;
+            default:
+                break;
+        }
+    });
+    $("#reset").click(function() {
+        if ($("input[name='radio']:checked")) {
+            $("input[name='radio']:checked").prop('checked', false);
+        }
+        $('#datepicker').datepick('destroy');
+    });
 }
-
-const formToJSON = formElements =>
-  [].reduce.call(
-    formElements,
-    (data, element) => {
-      if (isValidElement(element)) {
-        data[element.name] = element.value;
-      }
-      return data;
-    },
-    {}
-  );
-
-const isValidElement = element => {
-  return element.name && element.value;
-};
-
-const form = document.getElementsByClassName("main-form")[0];
-form.addEventListener("submit", handleSubmit);
+$(function() {
+    fetch('/widget?resource=datepicker.html').then(function(data) {
+        data.text().then(function(html) {
+            document.getElementById('datepicker-component').innerHTML = html;
+            enableDatepicker();
+        });
+    });
+});
