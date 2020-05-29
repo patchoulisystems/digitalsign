7483c877e71e91ceaf11c0866aac027471f22c4e
Vladimir Ventura <vladventura@outlook.es>
2020-05-28 23:33:21 -0400

    This is my local version of the project.I changed the app.js back to what it was before

--- client/components/datepicker.js
+++ client/components/datepicker.js
@@ -1,36 +1,70 @@
 var options = {
-    rangeSelect: true,
-    minDate: 0,
-    multiSelect: 0,
-    dateFormat: 'yyyy-mm-dd',
-    altField: '#dpfield'
+  rangeSelect: true,
+  minDate: 0,
+  multiSelect: null,
+  dateFormat: "yyyy-mm-dd"
 };
-$(document).ready(function() {
 
-    $("#reset").click(function() {
-        if ($("input[name='radio']:checked")) {
-            $("input[name='radio']:checked").prop('checked', false);
-        }
-        $('#datepicker').datepick('destroy');
-    });
+const startDatepicker = () => {
+  $("#datepicker").datepick(options);
+  attachOnChange();
+  onResetPress();
+};
 
-    $("input[name='radio']").on('change', function() {
-        var selected = $("input[name='radio']:checked").val();
-        switch (selected) {
-            case 'multiple':
-                options.rangeSelect = null;
-                options.multiSelect = 999;
-                $('#datepicker').datepick('destroy').datepick(options);
-                $('#dpfield').val('');
-                break;
-            case 'interval':
-                options.rangeSelect = true;
-                options.multiSelect = null;
-                $('#datepicker').datepick('destroy').datepick(options);
-                $('#dpfield').val('');
-                break;
-            default:
-                break;
-        }
+const attachOnChange = () => {
+  $("input[name='radio']").on("change", () => {
+    var selected = $("input[name='radio']:checked").val();
+    switch (selected) {
+      case "multiple":
+        options.rangeSelect = null;
+        options.multiSelect = 999;
+        $("#datepicker")
+          .datepick("destroy")
+          .datepick(options);
+        $("#dpfield").val("");
+        break;
+      case "interval":
+        options.rangeSelect = true;
+        options.multiSelect = null;
+        $("#datepicker")
+          .datepick("destroy")
+          .datepick(options);
+        $("#dpfield").val("");
+        break;
+      default:
+        break;
+    }
+  });
+};
+
+const onResetPress = () => {
+  $("#reset").click(() => {
+    if ($("input[name='radio']:checked")) {
+      $("input[name='radio']:checked").prop("checked", false);
+    }
+    $("#datepicker").datepick("destroy");
+  });
+};
+
+const parseDatepicker = () => {
+  let resultDateString = "";
+
+  let radioValue = $("input[name='radio']:checked").val();
+
+  if (radioValue == "interval") {
+    let dates = $("#datepicker").datepick("getDate");
+    let formatedLeft = $.datepick.formatDate("yyyy-mm-dd", dates[0]);
+    let formatedRight = $.datepick.formatDate("yyyy-mm-dd", dates[1]);
+    if (formatedLeft && formatedRight) resultDateString = `${formatedLeft} - ${formatedRight}`;
+  } else if (radioValue == "multiple") {
+    let dates = $("#datepicker").datepick("getDate");
+    dates.forEach(date => {
+      let formated = $.datepick.formatDate("yyyy-mm-dd", date);
+      resultDateString += formated;
+      if (date != dates[dates.length - 1]) {
+        resultDateString += ",";
+      }
     });
-});
+  }
+  return resultDateString;
+};
---
+++ client/edit_day_page/edit_day.js
@@ -0,0 +1,125 @@
+$(() => {
+  fetch("/widget?resource=datepicker.html").then(data => {
+    data.text().then(html => {
+      document.getElementById("datepicker-component").innerHTML = html;
+      startDatepicker();
+    });
+  });
+  $(".sendData").click(event => {
+    onSubmit();
+  });
+  openFrame();
+});
+
+const openFrame = () => {
+  let columns = $(".picture-column");
+  columns.innerHTML = "";
+  let builtImages = [];
+
+  $.get("/dated_images").then(response => {
+    response.data.forEach(image => {
+      let singlePicture = document.createElement("div");
+      singlePicture.setAttribute("class", "single-picture picture-unselected");
+      singlePicture.setAttribute("id", image);
+
+      singlePicture.onclick = function() {
+        onSinglePictureClick(image);
+      };
+
+      let imageTag = document.createElement("img");
+      imageTag.setAttribute("src", `/image?name=${image}`);
+
+      let overlay = document.createElement("div");
+      overlay.setAttribute("class", "overlay unselected");
+
+      singlePicture.appendChild(imageTag);
+      singlePicture.appendChild(overlay);
+
+      builtImages.push(singlePicture);
+    });
+
+    let counter = 0;
+    while (builtImages.length > 0) {
+      columns[counter % 4].appendChild(builtImages.pop());
+      counter++;
+    }
+  });
+  $("#frame").css({
+    transition: "height 0ms 0ms, opacity 600ms 0ms",
+    opacity: 1,
+    height: "auto"
+  });
+};
+
+const onSinglePictureClick = pictureName => {
+  let singlePicture = document.getElementById(pictureName);
+  let singlePictureOverlay = singlePicture.getElementsByClassName("overlay")[0];
+  togglePictureClass(singlePicture, singlePictureOverlay);
+};
+
+const togglePictureClass = (picture, overlay) => {
+  if (picture.classList.contains("picture-unselected")) {
+    picture.classList.remove("picture-unselected");
+    picture.classList.add("picture-selected");
+    overlay.classList.remove("unselected");
+    overlay.classList.add("selected");
+  } else {
+    picture.classList.remove("picture-selected");
+    picture.classList.add("picture-unselected");
+    overlay.classList.remove("selected");
+    overlay.classList.add("unselected");
+  }
+};
+
+const onSubmit = event => {
+  let selectedPicturesNames = [];
+  let selectedPicturesElements = Array.from(
+    document.getElementsByClassName("picture-selected")
+  );
+  let dateToSend = parseDatepicker();
+  let dateType = $("input[name='radio']:checked").val();
+  let errors = false;
+
+  if (selectedPicturesElements.length == 0) {
+    alert("Please select at least one picture!");
+    errors = true;
+  }
+
+  // Just to avoid one alert after another
+  if (dateToSend.length == 0 && errors == false) {
+    alert("Please finish the date selection!");
+    errors = true;
+  }
+
+  if (!errors) {
+    selectedPicturesElements.forEach(element => {
+      selectedPicturesNames.push(element.id);
+    });
+
+    let payload = {
+      method: "POST",
+      type: "POST",
+      url: "/piclist",
+      data: JSON.stringify({
+        dateType: dateType,
+        dates: dateToSend,
+        pictures: selectedPicturesNames
+      }),
+      contentType: "application/json",
+    };
+
+    $.ajax(payload)
+      .fail((xhr, error) => {
+        if (xhr.status == 400) {
+          alert(
+            "The request was unable to be completed. Please refresh the page or try again later."
+          );
+        }
+      })
+      .done((response, status, xhr) => {
+        if (xhr.status == 200) {
+          alert("Your image has been successfully submitted!");
+        }
+      });
+  }
+};
