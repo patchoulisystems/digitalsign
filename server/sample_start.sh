945257aa39e0a95da7a7050028300d532dd3a6ea
patchoulisystems <mcfyre@gmail.com>
2020-06-03 18:12:18 -0400

    server can now use https with a certcert may be specified in the sample_start.sh as an environment variable other variable in the sample_start.sh include listening IP/PORT, and passphrase for the cert, if needed

---
+++ server/sample_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#export PORT="portToListenOn"
+#export HOSTNAME="addressToListenOn"
+#export PFX="/path/to/your/myPfxFile.pfx"
+#export SECRET='the passphrase for your .pfx here, using single quotes to ignore escape char'
+
+nodemon app.js
