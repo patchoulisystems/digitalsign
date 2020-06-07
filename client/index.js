530315db37245f429675ee189172f051d43c588b
Vladimir Ventura <vladventura@outlook.es>
2020-06-07 13:21:51 -0400

    jQuery only method, we also reroute users to their destination from the button attribute

--- client/index.js
+++ client/index.js
@@ -1,9 +1,14 @@
 $(() => {
-  document.querySelector(".button").onmousemove = (e) => {
+  $(".button").mousemove((e) => {
     const x = e.pageX - e.target.offsetLeft;
     const y = e.pageY - e.target.offsetTop;
 
     e.target.style.setProperty("--x", `${x}px`);
     e.target.style.setProperty("--y", `${y}px`);
-  };
+  });
+
+  $(".button").click((event) => {
+    let sendTo = $(event.target).attr("sendTo");
+    window.open(sendTo);
+  });
 });
