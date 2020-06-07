f4bfcd5fd08968884ae75e609a1e78d34f113c8b
Vladimir Ventura <vladventura@outlook.es>
2020-06-07 09:04:43 -0400

    Abstracting the modal into its own file/set of files

---
+++ client/components/modal/modal.js
@@ -0,0 +1,20 @@
+var modal;
+var modalContent;
+
+const startModal = () => {
+  modal = $("#modal");
+  modalContent = $(modal).find('p[id="modal-text"]');
+  $('button[id="modal-close"]').click(() => {
+    modal.css("display", "none");
+    modalContent.text("");
+  });
+  $(window).click(() => {
+    modal.css("display", "none");
+    modalContent.text("");
+  });
+};
+
+const displayModal = (text) => {
+  modalContent.text(text);
+  modal.css("display", "block");
+};
