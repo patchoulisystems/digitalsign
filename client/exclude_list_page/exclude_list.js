8e0036ce8b1f301d91a6285f9721bd4e540e9811
Vladimir Ventura <vladventura@outlook.es>
2020-06-08 16:54:54 -0400

    WIP: Currently building the exclude list. We can request for dated images, now all that's left to do is recieve exclude lists, add them to the meta and then, whenever we're going to serve pictures remove the excluded ones. Also, whenever we're recieving a picture from the form or creating a new picture list we should take them off of the exclude. Basically, add them once and if they're added again remove them from the exclude

---
+++ client/exclude_list_page/exclude_list.js
@@ -0,0 +1,108 @@
+$(() => {
+  $(".getData").click((event) => {
+    closeFrame();
+    onRequest();
+  });
+  $(".sendData").click((event) => {
+    onSubmit();
+    closeFrame();
+  });
+});
+
+const closeFrame = () => {
+  $(".picture-column").html("");
+}
+
+const openFrame = (data) => {
+  let columns = $(".picture-column");
+  columns.innerHTML = "";
+  let builtImages = [];
+  data.forEach((image) => {
+    let singlePicture = document.createElement("div");
+    singlePicture.setAttribute("class", "single-picture picture-unselected");
+    singlePicture.setAttribute("id", image);
+
+    singlePicture.onclick = function () {
+      onSinglePictureClick(image);
+    };
+
+    let imageTag = document.createElement("img");
+    imageTag.setAttribute("src", `/image?name=${image}`);
+
+    let overlay = document.createElement("div");
+    overlay.setAttribute("class", "overlay unselected");
+
+    singlePicture.appendChild(imageTag);
+    singlePicture.appendChild(overlay);
+
+    builtImages.push(singlePicture);
+  });
+
+  let counter = 0;
+  while (builtImages.length > 0) {
+    columns[counter % 4].appendChild(builtImages.pop());
+    counter++;
+  }
+  $("#frame").css({
+    transition: "height 0ms 0ms, opacity 600ms 0ms",
+    opacity: 1,
+    height: "auto",
+  });
+};
+
+const onSinglePictureClick = (pictureName) => {
+  let singlePicture = document.getElementById(pictureName);
+  let singlePictureOverlay = singlePicture.getElementsByClassName("overlay")[0];
+  togglePictureClass(singlePicture, singlePictureOverlay);
+};
+
+const togglePictureClass = (picture, overlay) => {
+  if (picture.classList.contains("picture-unselected")) {
+    picture.classList.remove("picture-unselected");
+    picture.classList.add("picture-selected");
+    overlay.classList.remove("unselected");
+    overlay.classList.add("selected");
+  } else {
+    picture.classList.remove("picture-selected");
+    picture.classList.add("picture-unselected");
+    overlay.classList.remove("selected");
+    overlay.classList.add("unselected");
+  }
+};
+
+const onRequest = () => {
+  let dateToSend = parseDatepicker();
+  let dateType = $("input[name='radio']:checked").val();
+  let errors = false;
+
+  if (!dateToSend) {
+    alert("Please choose at least one day to request pictures from that date");
+    errors = true;
+  }
+
+  if (!errors) {
+    let payload = {
+      method: "POST",
+      type: "POST",
+      url: "/dated_images",
+      data: JSON.stringify({
+        dateType: dateType,
+        dates: dateToSend,
+      }),
+      contentType: "application/json",
+    };
+
+    $.ajax(payload)
+      .fail((xhr, error) => {
+        if (xhr.status == 400) {
+          alert(
+            "The request was unable to be completed. Please refresh the page or try again later."
+          );
+          clearDatepicker();
+        }
+      })
+      .done((response, status, xhr) => {
+        openFrame(response.data);
+      });
+  }
+};
