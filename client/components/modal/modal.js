0b468cc30b73174887ec53616a1e633a3f84a6da
Vlad Ventura <vladventura@outlook.es>
2020-06-13 01:28:53 -0400

    The solution to the issue was to remove the window's event listener because of the z-index of the picture grid needed to work messes up this index

--- client/components/modal/modal.js
+++ client/components/modal/modal.js
@@ -14,6 +14,15 @@ const startModal = () => {
   });
 };
 
+const startModalForList = () => {
+  modal = $("#modal");
+  modalContent = $(modal).find('p[id="modal-text"]');
+  $('button[id="modal-close"]').click(() => {
+    modal.css("display", "none");
+    modalContent.text("");
+  });
+};
+
 const displayModal = (text) => {
   modalContent.text(text);
   modal.css("display", "block");
