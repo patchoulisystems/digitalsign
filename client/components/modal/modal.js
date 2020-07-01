52fda0396c8bec8f7e198a956ee6c3d7e412a3c2
Vladimir Ventura <vladventura@outlook.es>
2020-06-30 20:23:53 -0400

    We can now pass in a function for the button's onclick, each button's text and also we're using the more versatile on/off event attachers for the elements. So basically, if we pass a function we can attach that to the main button and render two buttons at a time, one that will do what we pass, and the other one that will just close the modal. For now, and for our use case, makes sense that we close the modal right here. We're clearing the previous onclick fromthe main button because if we don't, then we just append to the previous one, and then our functions will run more than once

--- client/components/modal/modal.js
+++ client/components/modal/modal.js
@@ -4,26 +4,49 @@ var modalContent;
 const startModal = () => {
   modal = $("#modal");
   modalContent = $(modal).find('p[id="modal-text"]');
-  $('button[id="modal-close"]').click(() => {
-    modal.css("display", "none");
-    modalContent.text("");
+  $('button[id="modal-action"]').on("click", () => {
+    closeModal();
   });
   $(window).click(() => {
-    modal.css("display", "none");
-    modalContent.text("");
+    closeModal();
   });
+};
+
+const closeModal = () => {
+  modal.css("visibility", "hidden");
+  modalContent.text("");
 };
 
 const startModalForList = () => {
   modal = $("#modal");
   modalContent = $(modal).find('p[id="modal-text"]');
-  $('button[id="modal-close"]').click(() => {
-    modal.css("display", "none");
-    modalContent.text("");
+  $('button[id="modal-action"]').on("click", () => {
+    closeModal();
   });
 };
 
-const displayModal = (text) => {
+const displayModal = (text, fn, buttonAText, buttonBText) => {
+  $("button#modal-cancel").remove();
+  $('button[id="modal-action"] > span').html(buttonAText || "Ok");
+  if (fn) {
+    $("#modal > div > div.button-group")
+      .prepend(
+        "<button id='modal-cancel' class='button close input'>" +
+          (buttonBText || "Cancel") +
+          "</button>"
+      )
+      .click(() => closeModal());
+    $("button#modal-action")
+      .off("click")
+      .on("click", () => {
+        fn();
+        closeModal();
+      });
+  } else {
+    $("button#modal-action")
+      .off("click")
+      .on("click", () => closeModal);
+  }
   modalContent.text(text);
-  modal.css("display", "block");
+  modal.css("visibility", "visible");
 };
