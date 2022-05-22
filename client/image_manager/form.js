ff7eabf8c610b9634609bea238d3dbf0bb27b7a9
patchouli <mcfyre@gmail.com>
2022-05-22 19:25:35 -0400

    initial commit for imagemanager

---
+++ client/image_manager/form.js
@@ -0,0 +1,69 @@
+$(() => {
+  fetch("/widget?widgetName=datepicker&resource=datepicker.html").then(
+    (data) => {
+      data.text().then((html) => {
+        let ogHTML = document.getElementById("datepicker-component").innerHTML;
+        document.getElementById("datepicker-component").innerHTML =
+          html + ogHTML;
+        startDatepicker();
+        fetch("/widget?widgetName=modal&resource=modal.html").then((data) => {
+          data.text().then((html) => {
+            $("#modal").html(html);
+            startModal();
+            startGlitter();
+          });
+        });
+      });
+    }
+  );
+});
+
+$(document).on("submit", "form", (event) => {
+  event.preventDefault();
+  let form = document.forms.namedItem("main-form");
+  let formData = new FormData(form);
+  let errors = false;
+
+  if (!formData.get("firstname")) {
+    displayModal("First Name is required");
+    errors = true;
+  } else if (!formData.get("lastname")) {
+    displayModal("Last Name is required");
+    errors = true;
+  } else if (!formData.get("studentid")) {
+    displayModal("Student ID is required");
+    errors = true;
+  } else if (!formData.get("picture").name) {
+    displayModal("A picture to upload is required!");
+    errors = true;
+  } else if (!parseDatepicker()) {
+    displayModal("Please finish choosing your dates!");
+    errors = true;
+  }
+
+  if (!errors) {
+    formData.append("dates", parseDatepicker());
+    $.ajax({
+      url: form.action,
+      type: "POST",
+      method: form.method,
+      data: formData,
+      contentType: false,
+      processData: false,
+    })
+      .fail((xhr, error) => {
+        if (xhr.status == 400) {
+          displayModal(
+            "The request was unable to be completed. Please refresh the page or try again later."
+          );
+        }
+      })
+      .done((response, status, xhr) => {
+        if (xhr.status == 200) {
+          displayModal("Your image has been successfully submitted!");
+          form.reset();
+          clearDatepicker();
+        }
+      });
+  }
+});
