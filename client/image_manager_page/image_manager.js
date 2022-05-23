5e9bfeb79194158ea3960b75367a8d4f2a16ee4e
patchouli <mcfyre@gmail.com>
2022-05-22 20:01:53 -0400

    added image columns to image manager

--- client/image_manager_page/image_manager.js
+++ client/image_manager_page/image_manager.js
@@ -1,69 +1,131 @@
 $(() => {
-  fetch("/widget?widgetName=datepicker&resource=datepicker.html").then(
-    (data) => {
-      data.text().then((html) => {
-        let ogHTML = document.getElementById("datepicker-component").innerHTML;
-        document.getElementById("datepicker-component").innerHTML =
-          html + ogHTML;
-        startDatepicker();
-        fetch("/widget?widgetName=modal&resource=modal.html").then((data) => {
-          data.text().then((html) => {
-            $("#modal").html(html);
-            startModal();
-            startGlitter();
+  (data) => {
+    data.text().then((html) => {
+      fetch("/widget?widgetName=modal&resource=modal.html").then((data) => {
+        data.text().then((html) => {
+          $("#modal").html(html);
+          startModalForList();
+          $(".sendData").click((event) => {
+            onSubmit();
           });
+          startGlitter();
         });
       });
-    }
-  );
+    });
+  }
+  openFrame();
 });
 
-$(document).on("submit", "form", (event) => {
-  event.preventDefault();
-  let form = document.forms.namedItem("main-form");
-  let formData = new FormData(form);
+const openFrame = () => {
+  let columns = $(".picture-column");
+  columns.innerHTML = "";
+  let builtImages = [];
+
+  $.get("/dated_images").then((response) => {
+    response.data.forEach((image) => {
+      let singlePicture = document.createElement("div");
+      singlePicture.setAttribute("class", "single-picture picture-unselected");
+      singlePicture.setAttribute("id", image);
+
+      singlePicture.onclick = function () {
+        onSinglePictureClick(image);
+      };
+
+      let imageTag = document.createElement("img");
+      imageTag.setAttribute("src", `/image?name=${image}`);
+
+      let overlay = document.createElement("div");
+      overlay.setAttribute("class", "overlay unselected");
+
+      singlePicture.appendChild(imageTag);
+      singlePicture.appendChild(overlay);
+
+      builtImages.push(singlePicture);
+    });
+
+    let counter = 0;
+    while (builtImages.length > 0) {
+      columns[counter % 4].appendChild(builtImages.pop());
+      counter++;
+    }
+  });
+  $("#frame").css({
+    transition: "height 0ms 0ms, opacity 600ms 0ms",
+    opacity: 1,
+    height: "auto",
+  });
+};
+
+const onSinglePictureClick = (pictureName) => {
+  let singlePicture = document.getElementById(pictureName);
+  let singlePictureOverlay = singlePicture.getElementsByClassName("overlay")[0];
+  togglePictureClass(singlePicture, singlePictureOverlay);
+};
+
+const togglePictureClass = (picture, overlay) => {
+  if (picture.classList.contains("picture-unselected")) {
+    picture.classList.remove("picture-unselected");
+    picture.classList.add("picture-selected");
+    overlay.classList.remove("unselected");
+    overlay.classList.add("selected");
+  } else {
+    picture.classList.remove("picture-selected");
+    picture.classList.add("picture-unselected");
+    overlay.classList.remove("selected");
+    overlay.classList.add("unselected");
+  }
+};
+
+const onSubmit = (event) => {
+  let selectedPicturesNames = [];
+  let selectedPicturesElements = Array.from(
+    document.getElementsByClassName("picture-selected")
+  );
+  let dateToSend = parseDatepicker();
+  let dateType = $("input[name='radio']:checked").val();
   let errors = false;
 
-  if (!formData.get("firstname")) {
-    displayModal("First Name is required");
+  if (selectedPicturesElements.length == 0) {
+    displayModal("Please select at least one picture!");
     errors = true;
-  } else if (!formData.get("lastname")) {
-    displayModal("Last Name is required");
-    errors = true;
-  } else if (!formData.get("studentid")) {
-    displayModal("Student ID is required");
-    errors = true;
-  } else if (!formData.get("picture").name) {
-    displayModal("A picture to upload is required!");
-    errors = true;
-  } else if (!parseDatepicker()) {
-    displayModal("Please finish choosing your dates!");
+  }
+
+  if (dateToSend.length == 0 && errors == false) {
+    displayModal("Please finish the date selection!");
     errors = true;
   }
 
   if (!errors) {
-    formData.append("dates", parseDatepicker());
-    $.ajax({
-      url: form.action,
+    selectedPicturesElements.forEach((element) => {
+      selectedPicturesNames.push(element.id);
+    });
+
+    let payload = {
+      method: "POST",
       type: "POST",
-      method: form.method,
-      data: formData,
-      contentType: false,
-      processData: false,
-    })
+      url: "/piclist",
+      data: JSON.stringify({
+        dateType: dateType,
+        dates: dateToSend,
+        pictures: selectedPicturesNames,
+      }),
+      contentType: "application/json",
+    };
+
+    $.ajax(payload)
       .fail((xhr, error) => {
         if (xhr.status == 400) {
           displayModal(
             "The request was unable to be completed. Please refresh the page or try again later."
           );
+          clearDatepicker();
         }
       })
       .done((response, status, xhr) => {
         if (xhr.status == 200) {
           displayModal("Your image has been successfully submitted!");
-          form.reset();
           clearDatepicker();
         }
       });
   }
-});
+};
